33d19e8d10f34c0faf2bf92face3a75d1d384fec
stedel <steffen.meifort@gmail.com>
2017-05-21 11:01:06 +0200

    Add EA4

--- ea1/ea1.js
+++ ea1/ea1.js
@@ -13,22 +13,46 @@ let radian = 0;
 
 const SPRITESHEET_FRAMETIME = 200;
 const SPRITESHEET_POSITIONS = [
-    ["0px", "0px"],
-    ["0px", "-400px"],
-    ["0px", "-800px"],
-    ["0px", "-1200px"],
-    ["-400px", "-1200px"],
-    ["-400px", "-800px"],
-    ["-400px", "-400px"],
-    ["-400px", "0px"]
+  ["0px", "0px"],
+  ["0px", "-400px"],
+  ["0px", "-800px"],
+  ["0px", "-1200px"],
+  ["-400px", "-1200px"],
+  ["-400px", "-800px"],
+  ["-400px", "-400px"],
+  ["-400px", "0px"]
 ];
-const ANIMATION_SEQUENCE = [0, 1, 2, 3, 2, 1, 2, 3, 0, 0, 0, 4, 4, 5, 6, 7, 6, 5, 6, 7, 4, 4, 0];
+const ANIMATION_SEQUENCE = [
+  0,
+  1,
+  2,
+  3,
+  2,
+  1,
+  2,
+  3,
+  0,
+  0,
+  0,
+  4,
+  4,
+  5,
+  6,
+  7,
+  6,
+  5,
+  6,
+  7,
+  4,
+  4,
+  0
+];
 
 let currentAnimationFrame = 0;
 let spritesheetAnimationIntervalId = 0;
 
-$(function () {
-    $(document).keydown(keyPressed);
+$(function() {
+  $(document).keydown(keyPressed);
 });
 
 /**
@@ -37,17 +61,17 @@ $(function () {
  * @param key the pressed key
  */
 function keyPressed(key) {
-    switch (key.keyCode) {
-        case KEY_L:
-            rotate(true);
-            break;
-        case KEY_R:
-            rotate(false);
-            break;
-        case KEY_SPACE:
-            startSpriteAnimation();
-            break;
-    }
+  switch (key.keyCode) {
+    case KEY_L:
+      rotate(true);
+      break;
+    case KEY_R:
+      rotate(false);
+      break;
+    case KEY_SPACE:
+      startSpriteAnimation();
+      break;
+  }
 }
 
 /**
@@ -56,19 +80,19 @@ function keyPressed(key) {
  * @param clockwise specifies, if the rotation should be clockwise or anti-clockwise
  */
 function rotate(clockwise) {
-    if (clockwise) {
-        radian += RADIAN_CHANGE;
-        if (radian >= 360) {
-            radian = 0;
-        }
-    } else {
-        radian -= RADIAN_CHANGE;
-        if (radian < 0) {
-            radian = 360 - RADIAN_CHANGE;
-        }
+  if (clockwise) {
+    radian += RADIAN_CHANGE;
+    if (radian >= 360) {
+      radian = 0;
     }
+  } else {
+    radian -= RADIAN_CHANGE;
+    if (radian < 0) {
+      radian = 360 - RADIAN_CHANGE;
+    }
+  }
 
-    updateCircle(radian);
+  updateCircle(radian);
 }
 
 /**
@@ -77,34 +101,40 @@ function rotate(clockwise) {
  * @param radian the radian to be displayed
  */
 function updateCircle(radian) {
-    $("#circle").attr("src", "images/circle_" + radian + ".png");
+  $("#circle").attr("src", "images/circle_" + radian + ".png");
 }
 
 /**
  * Starts the sprite animation if it is not running
  */
 function startSpriteAnimation() {
-    if (currentAnimationFrame === 0) {
-        spritesheetAnimationIntervalId = setInterval(animateSprite, SPRITESHEET_FRAMETIME);
-    }
+  if (currentAnimationFrame === 0) {
+    spritesheetAnimationIntervalId = setInterval(
+      animateSprite,
+      SPRITESHEET_FRAMETIME
+    );
+  }
 }
 
 /**
  * Loops through ANIMATION_SEQUENCE and sets current display to each corresponding spritesheet position
  */
 function animateSprite() {
-    if (currentAnimationFrame < ANIMATION_SEQUENCE.length - 1) {
-        currentAnimationFrame++;
-    } else {
-        currentAnimationFrame = 0;
+  if (currentAnimationFrame < ANIMATION_SEQUENCE.length - 1) {
+    currentAnimationFrame++;
+  } else {
+    currentAnimationFrame = 0;
 
-        if (spritesheetAnimationIntervalId !== 0) {
-            clearInterval(spritesheetAnimationIntervalId);
-        }
+    if (spritesheetAnimationIntervalId !== 0) {
+      clearInterval(spritesheetAnimationIntervalId);
     }
+  }
 
-    const currentSpritePositionIndex = ANIMATION_SEQUENCE[currentAnimationFrame];
-    updateSpriteCss(SPRITESHEET_POSITIONS[currentSpritePositionIndex][0], SPRITESHEET_POSITIONS[currentSpritePositionIndex][1]);
+  const currentSpritePositionIndex = ANIMATION_SEQUENCE[currentAnimationFrame];
+  updateSpriteCss(
+    SPRITESHEET_POSITIONS[currentSpritePositionIndex][0],
+    SPRITESHEET_POSITIONS[currentSpritePositionIndex][1]
+  );
 }
 
 /**
@@ -114,6 +144,6 @@ function animateSprite() {
  * @param left left offset
  */
 function updateSpriteCss(top, left) {
-    $("#spritesheet").css('top', top);
-    $("#spritesheet").css('left', left);
+  $("#spritesheet").css("top", top);
+  $("#spritesheet").css("left", left);
 }
--- ea2/ea2.js
+++ ea2/ea2.js
@@ -4,116 +4,143 @@
 
 "use strict";
 
-$(function () {
-    createGraphic();
+$(function() {
+  createGraphic();
 });
 
 function createGraphic() {
+  const canvas = document.getElementById("canvas");
+  const gl = initWebGL(canvas);
 
-    const canvas = document.getElementById("canvas");
-    const gl = initWebGL(canvas);
+  if (gl) {
+    // setup
+    gl.clearColor(0.764, 0.925, 0.917, 1); // Clear background default color
+    gl.clearDepth(1.0); // Clear everything
+    gl.enable(gl.DEPTH_TEST); // Enable depth testing
+    gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    if (gl) {
-        // setup
-        gl.clearColor(0.764, 0.925, 0.917, 1);  // Clear background default color
-        gl.clearDepth(1.0);                 // Clear everything
-        gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-        gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+    // compile vertex shader
+    const vsSource =
+      "attribute vec2 pos;" +
+      "void main(){gl_Position = vec4(pos * 1.75, 0, 1); }";
+    const vs = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vs, vsSource);
+    gl.compileShader(vs);
 
-        // compile vertex shader
-        const vsSource = 'attribute vec2 pos;' +
-            'void main(){gl_Position = vec4(pos * 1.75, 0, 1); }';
-        const vs = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vs, vsSource);
-        gl.compileShader(vs);
+    // compile fragment shader
+    const fsSouce = "void main() { gl_FragColor = vec4(0.2, 0.2, 0.2, 1); }";
+    const fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fsSouce);
+    gl.compileShader(fs);
 
-        // compile fragment shader
-        const fsSouce = 'void main() { gl_FragColor = vec4(0.2, 0.2, 0.2, 1); }';
-        const fs = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fs, fsSouce);
-        gl.compileShader(fs);
+    // link program
+    const prog = gl.createProgram();
+    gl.attachShader(prog, vs);
+    gl.attachShader(prog, fs);
+    gl.linkProgram(prog);
+    gl.useProgram(prog);
 
-        // link program
-        const prog = gl.createProgram();
-        gl.attachShader(prog, vs);
-        gl.attachShader(prog, fs);
-        gl.linkProgram(prog);
-        gl.useProgram(prog);
+    const shutterOutside = new Float32Array([
+      0.5,
+      0,
+      0.4,
+      0.3,
+      0.2,
+      0.45,
+      0,
+      0.5,
+      -0.2,
+      0.45,
+      -0.4,
+      0.3,
+      -0.5,
+      0,
+      -0.4,
+      -0.3,
+      -0.2,
+      -0.45,
+      0,
+      -0.5,
+      0.2,
+      -0.45,
+      0.4,
+      -0.3
+    ]);
 
-        const shutterOutside = new Float32Array([
-            0.5, 0, 0.4, 0.3, 0.2, 0.45,
-            0, 0.5, -0.2, 0.45, -0.4, 0.3,
-            -0.5, 0, -0.4, -0.3, -0.2, -0.45,
-            0, -0.5, 0.2, -0.45, 0.4, -0.3
-        ]);
+    const shutterBlade = new Float32Array([
+      0.05,
+      -0.1,
+      0.4,
+      0.1,
+      0.45,
+      0,
+      0.4,
+      -0.1
+    ]);
 
-        const shutterBlade = new Float32Array([
-            0.05, -0.1, 0.4, 0.1, 0.45, 0, 0.4, -0.1
-        ]);
+    const vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, shutterOutside, gl.STATIC_DRAW);
 
-        const vbo = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-        gl.bufferData(gl.ARRAY_BUFFER, shutterOutside, gl.STATIC_DRAW);
+    const posAttrib = gl.getAttribLocation(prog, "pos");
+    gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(posAttrib);
 
-        const posAttrib = gl.getAttribLocation(prog, 'pos');
-        gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(posAttrib);
+    gl.drawArrays(gl.LINE_LOOP, 0, 12);
 
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.LINE_LOOP, 0, 12);
+    const shutterBladeCount = 12;
 
-        const shutterBladeCount = 12;
+    // radians conversion + difference between each shutterBlade
+    const rotationDegrees = Math.PI / 180 * (360 / shutterBladeCount);
 
-        // radians conversion + difference between each shutterBlade
-        const rotationDegrees = (Math.PI / 180) * (360 / shutterBladeCount);
+    let nextShutterBlade = shutterBlade;
+    for (let i = 0; i < shutterBladeCount; i++) {
+      gl.bufferData(gl.ARRAY_BUFFER, nextShutterBlade, gl.STATIC_DRAW);
+      gl.drawArrays(gl.LINE_LOOP, 0, 4);
 
-        let nextShutterBlade = shutterBlade;
-        for (let i = 0; i < shutterBladeCount; i++) {
-            gl.bufferData(gl.ARRAY_BUFFER, nextShutterBlade, gl.STATIC_DRAW);
-            gl.drawArrays(gl.LINE_LOOP, 0, 4);
-
-            nextShutterBlade = rotate(nextShutterBlade, rotationDegrees);
-        }
+      nextShutterBlade = rotate(nextShutterBlade, rotationDegrees);
     }
+  }
 
-    /**
+  /**
      * Rotates the given Float32Array counterclockwise around the center (0,0).
      * @param {Float32Array} toBeRotated
      * @param {number} degrees
      * @returns {Float32Array} rotated array
      */
-    function rotate(toBeRotated, degrees) {
-        let rotated = new Float32Array(toBeRotated.length);
+  function rotate(toBeRotated, degrees) {
+    let rotated = new Float32Array(toBeRotated.length);
 
-        for (let i = 0; i < toBeRotated.length; i = i + 2) {
-            const x = toBeRotated[i];
-            const y = toBeRotated[i+1];
+    for (let i = 0; i < toBeRotated.length; i = i + 2) {
+      const x = toBeRotated[i];
+      const y = toBeRotated[i + 1];
 
-            rotated[i] = x*Math.cos(degrees) - y*Math.sin(degrees);
-            rotated[i+1] = y*Math.cos(degrees) + x*Math.sin(degrees);
-        }
-
-        return rotated;
+      rotated[i] = x * Math.cos(degrees) - y * Math.sin(degrees);
+      rotated[i + 1] = y * Math.cos(degrees) + x * Math.sin(degrees);
     }
 
-    /**
+    return rotated;
+  }
+
+  /**
      * Tries to create a WebGL context on the given canvas.
      * @param {HTMLCanvasElement} canvas - Canvas on which the context will be created
      * @returns {WebGLRenderingContext} Either a context or null
      */
-    function initWebGL(canvas) {
-        let context = null;
+  function initWebGL(canvas) {
+    let context = null;
 
-        try {
-            context = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-        } catch (e) {
-        }
+    try {
+      context =
+        canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    } catch (e) {}
 
-        if (!context) {
-            alert("WebGL konnte nicht initialisiert werden.");
-            context = null;
-        }
-
-        return context;
+    if (!context) {
+      alert("WebGL konnte nicht initialisiert werden.");
+      context = null;
     }
+
+    return context;
+  }
 }
